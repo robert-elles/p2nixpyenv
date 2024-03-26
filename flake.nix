a2ff27516914128793a1fe14bfcbe78afa719200
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2024-03-26 10:17:20 +0100

    cleanup

--- flake.nix
+++ flake.nix
@@ -8,24 +8,16 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     let
       py_version = "python311";
       pypkgs = "python311Packages";
-      patchedPkgs = nixpkgs.legacyPackages.x86_64-linux.applyPatches {
-        name = "nixpkgs-patched";
-        src = nixpkgs;
-        patches = [
-        ];
-      };
     in
     (flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import patchedPkgs {
+        pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
-          config.allowBroken = true;
           overlays = [ poetry2nix.overlays.default ];
         };
         inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
@@ -36,7 +28,7 @@
           myenv = mkPoetryEnv
             {
               projectDir = ./.;
-              preferWheels = true;
+              # preferWheels = true;
               python = pkgs.${py_version};
               extraPackages = pypkgs: with pypkgs; [
                 pip
@@ -44,25 +36,25 @@
 
               overrides = pkgs.poetry2nix.overrides.withDefaults (pyfinal: pyprev:
                 {
-                  # tokenizers = pyprev.tokenizers.override {
-                  #   preferWheel = true;
-                  # };
+                  tokenizers = pyprev.tokenizers.override {
+                    preferWheel = true;
+                  };
 
-                  # sentencepiece = pyprev.sentencepiece.override {
-                  #   preferWheel = true;
-                  # };
+                  sentencepiece = pyprev.sentencepiece.override {
+                    preferWheel = true;
+                  };
 
-                  # torch = pyprev.torch.override {
-                  #   preferWheel = true;
-                  # };
+                  torch = pyprev.torch.override {
+                    preferWheel = true;
+                  };
 
-                  # torch-struct = pyprev.torch-struct.override {
-                  #   preferWheel = true;
-                  # };
+                  torch-struct = pyprev.torch-struct.override {
+                    preferWheel = true;
+                  };
 
-                  # safetensors = pyprev.safetensors.override {
-                  #   preferWheel = true;
-                  # };
+                  safetensors = pyprev.safetensors.override {
+                    preferWheel = true;
+                  };
 
                   nvidia-cusparse-cu12 = (pyprev.nvidia-cusparse-cu12.override {
                     preferWheel = true;
@@ -71,15 +63,12 @@
                     propagatedBuildInputs = ((old.propagatedBuildInputs or [ ]) ++ [
                       pkgs.cudaPackages.cudatoolkit
                     ]);
-                    # src = pyprev.nvidia-cusparse-cu12.src;
                   });
 
                 });
             };
         };
-
         devShells.default = pkgs.mkShell {
-          NIX_PATH = "nixpkgs=${patchedPkgs}";
           buildInputs = [ ];
           packages = [
             self.packages.${system}.myenv
